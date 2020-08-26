12d0a73ca3df05d314a9785420675c6da87971eb
KPX <dm.mikufans@gmail.com>
2020-08-26 22:12:26 +0800

    #coc7

--- TheBot/DiceModule/DiceModule.fs
+++ TheBot/DiceModule/DiceModule.fs
@@ -1,4 +1,4 @@
-module TheBot.Module.DiceModule
+module TheBot.Module.DiceModule.DiceModule
 
 open System
 open KPX.FsCqHttp.Api
@@ -9,100 +9,7 @@ open TheBot.Utils.GenericRPN
 open TheBot.Utils.Dicer
 open TheBot.Utils.TextTable
 
-module ChoiceHelper =
-    open System.Text.RegularExpressions
-    open System.Collections.Generic
-
-    let YesOrNoRegex = Regex("(.*)(.+)([没不]\2)(.*)", RegexOptions.Compiled)
-
-    type MessageParser() = 
-        
-        static let dateTable = 
-            [|
-                "大后天", TimeSpan.FromDays(3.0)
-                "大前天", TimeSpan.FromDays(-2.0)
-                "明天", TimeSpan.FromDays(1.0)
-                "后天", TimeSpan.FromDays(2.0)
-                "昨天", TimeSpan.FromDays(-1.0)
-                "前天", TimeSpan.FromDays(-1.0)
-                "", TimeSpan.Zero
-            |]
-
-        member val AllowAtUser = true with get, set
-
-        member x.Parse(arg : CommandArgs) =
-            let atUser = 
-                arg.MessageEvent.Message.GetAts()
-                |> Array.tryHead
-                |> Option.filter (fun _ -> x.AllowAtUser)
-
-            [
-                for arg in arg.Arguments do 
-                    let seed = List<SeedOption>()
-                    let choices = List<string>()
-
-                    if atUser.IsSome then 
-                        let at = atUser.Value
-                        if at = Message.AtUserType.All then invalidOp "at全体无效"
-                        seed.Add(SeedOption.SeedCustom(atUser.Value.ToString()))
-
-                    //拆分条目
-                    let m = YesOrNoRegex.Match(arg)
-                    if m.Success then
-                        choices.Add(m.Groups.[1].Value + m.Groups.[2].Value + m.Groups.[4].Value)
-                        choices.Add(m.Groups.[1].Value + m.Groups.[3].Value + m.Groups.[4].Value)
-                    else
-                        choices.Add(arg)
-
-
-                    //处理选项
-                    for c in choices do 
-                        let seed = 
-                            [|
-                                yield! seed
-                                let (_, ts) = dateTable |> Array.find (fun x -> c.Contains(fst x))
-                                yield SeedOption.SeedCustom((GetCstTime() + ts).ToString("yyyyMMdd"))
-                            |]
-                        yield seed, c
-            ]
-
-module DiceExpression =
-    open System.Globalization
-
-    type DicerOperand(i : float) =
-        member x.Value = i
-
-        interface IOperand<DicerOperand> with
-            override l.Add(r) = DicerOperand(l.Value + r.Value)
-            override l.Sub(r) = DicerOperand(l.Value - r.Value)
-            override l.Div(r) = DicerOperand(l.Value / r.Value)
-            override l.Mul(r) = DicerOperand(l.Value * r.Value)
-
-        override x.ToString() = 
-            let fmt =
-                if (i % 1.0) <> 0.0 then "{0:N2}"
-                else "{0:N0}"
-            System.String.Format(fmt, i)
-
-    type DiceExpression() as x =
-        inherit GenericRPNParser<DicerOperand>()
-
-        let dicer = Dicer(SeedOption.SeedRandom)
-
-        do
-            let func (l : DicerOperand) (r : DicerOperand) =
-                let ret = Array.init<int> (l.Value |> int) (fun _ -> dicer.GetRandom(r.Value |> uint32)) |> Array.sum
-                DicerOperand(ret |> float)
-
-            x.Operatos.Add(GenericOperator<_>('D', Int32.MaxValue, func))
-            x.Operatos.Add(GenericOperator<_>('d', Int32.MaxValue, func))
-
-        override x.Tokenize(token) =
-            let succ, number = Double.TryParse(token)
-            if succ then
-                Operand(DicerOperand(number))
-            else
-                failwithf "无法解析 %s" token
+open TheBot.Module.DiceModule.Utils
 
 type DiceModule() =
     inherit CommandHandlerBase()
---
+++ TheBot/DiceModule/TRpgModule.fs
@@ -0,0 +1,40 @@
+﻿module TheBot.Module.DiceModule.TRpgModule
+
+open System
+open KPX.FsCqHttp.Api
+open KPX.FsCqHttp.DataType
+open KPX.FsCqHttp.Handler.CommandHandlerBase
+
+open TheBot.Utils.GenericRPN
+open TheBot.Utils.Dicer
+open TheBot.Utils.TextTable
+
+open TheBot.Module.DiceModule.Utils
+
+type TRpgModule() =
+    inherit CommandHandlerBase()
+
+
+    [<CommandHandlerMethodAttribute("coc7", "", "")>]
+    member x.HandleCoc7(msgArg : CommandArgs) =
+        let attrs = [|  "力量", "3D6*5"
+                        "体质", "3D6*5"
+                        "体型", "(2D6+6)*5"
+                        "敏捷", "3D6*5"
+                        "外貌", "3D6*5"
+                        "智力", "(2D6+6)*5"
+                        "意志", "3D6*5"
+                        "教育", "(2D6+6)*5"
+                        "幸运", "3D6*5"   |]
+
+        let tt = TextTable.FromHeader([|"属性"; "值"|])
+
+        let dicer = Dicer(SeedOption.SeedByUserDay(msgArg.MessageEvent))
+
+        let dp = DiceExpression.DiceExpression(dicer)
+
+        for (name, expr) in attrs do 
+            tt.AddRow(name, dp.Eval(expr).Value |> int)
+
+        tt.AddPreTable(sprintf "%s的人物作成:" msgArg.MessageEvent.GetNicknameOrCard)
+        using (msgArg.OpenResponse(true)) (fun ret -> ret.Write(tt))
