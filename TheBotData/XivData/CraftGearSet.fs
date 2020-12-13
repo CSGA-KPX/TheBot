73e0ced5952660996109bd90d4f0806553dca8bd
KPX <dm.mikufans@gmail.com>
2020-12-13 17:08:04 +0800

    FF14的CraftGearSet不再依赖其他数据表。

--- TheBotData/XivData/CraftGearSet.fs
+++ TheBotData/XivData/CraftGearSet.fs
@@ -25,7 +25,7 @@ type CraftableGearCollection private () =
     static let instance = CraftableGearCollection()
     static member Instance = instance
 
-    override x.Depends = [| typeof<CraftRecipeProvider> ; typeof<CompanyCraftRecipeProvider>|]
+    override x.Depends = Array.empty
 
     override x.IsExpired = false
 
@@ -34,7 +34,9 @@ type CraftableGearCollection private () =
         db.EnsureIndex(LiteDB.BsonExpression.Create("_id"), true) |> ignore
         db.EnsureIndex(LiteDB.BsonExpression.Create("ItemLv"), false) |> ignore
 
-        let fields = [|"EquipSlotCategory"; "Level{Equip}"; "Name"; "ClassJobCategory"; "Level{Item}"|]
+        let fields = [| "EquipSlotCategory"; "IsUntradable";
+                        "Level{Equip}"; "ClassJobCategory";
+                        "Level{Item}"; "CanBeHq";|]
         use col = BotDataInitializer.XivCollectionChs
         let chs = col.GetSheet("Item", fields)
         
@@ -50,24 +52,20 @@ type CraftableGearCollection private () =
                     yield row.Key.Main, String.Join(" ", j)
             |] |> readOnlyDict
         
-        let rm = XivRecipeManager.Instance
-        let ic = ItemCollection.Instance
         seq {
             for item in chs do 
-                let eq = item.As<int>("EquipSlotCategory") <> 0
-                let le = (item.As<int>("Level{Equip}") % 10) = 0
-                if eq && le then
-                    let cf = 
-                        let item = ic.GetByKey(item.Key.Main)
-                        let recipe = rm.TryGetRecipe(item)
-                        recipe.IsSome && recipe.Value.Input.Length > 0
-                    if cf then
-                        yield {
-                            Id = item.Key.Main
-                            ItemLv = item.As<int>("Level{Item}")
-                            EquipSlotCategory = item.As<int>("EquipSlotCategory")
-                            ClassJobCategory = ClassJobCategory.[item.As<int>("ClassJobCategory")]
-                        }
+                let elv = item.As<int>("Level{Equip}")
+                if (elv >= 80)
+                    && ((elv % 10) = 0)
+                    && (not <| item.As<bool>("IsUntradable"))
+                    && (item.As<int>("Level{Item}") >= 340 )
+                    && (item.As<bool>("CanBeHq")) then
+                    yield {
+                        Id = item.Key.Main
+                        ItemLv = item.As<int>("Level{Item}")
+                        EquipSlotCategory = item.As<int>("EquipSlotCategory")
+                        ClassJobCategory = ClassJobCategory.[item.As<int>("ClassJobCategory")]
+                    }
         }
         |> db.InsertBulk
         |> ignore
