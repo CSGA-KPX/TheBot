22504260de618102e4e17428f047c9397a186393
KPX <dm.mikufans@gmail.com>
2021-06-06 00:14:21 +0800

    gh-pages生成测试。

--- build.cmd
+++ build.cmd
@@ -8,3 +8,10 @@ dotnet build
 dotnet test /p:AltCover=true "/p:AltCoverTypeFilter=?KPX|ProviderImplementation.ProvidedTypes|LibFFXIV.GameData.Provider|StartupCode" "/p:AltCoverAssemblyExcludeFilter=Test|test" "/p:AltCoverReport=$(SolutionDir)/tmp/coverage_report/$(ProjectName).coverage.xml"
 
 dotnet reportgenerator "-reports:./tmp/coverage_report/*.xml" "-targetdir:./tmp/coverage_html/" "-sourcedirs:./src/;../LibFFXIV/" -reporttypes:Html
+
+rem git clone -b "gh-pages" . tmp/gh-pages
+rem xcopy /Y /E output\ tmp\gh-pages\
+rem pushd tmp\gh-pages
+rem git commit -a -m "Update generated documentation."
+rem git push
+rem popd
