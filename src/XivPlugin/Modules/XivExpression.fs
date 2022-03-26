3adccf54190289b7db07acc4879ccc8e52c1647e
KPX <dm.mikufans@gmail.com>
2022-03-26 17:26:17 +0800

    XivPlugin : 在XivExpression中使用@替代#cgss指令。

--- src/XivPlugin/Modules/XivExpression.fs
+++ src/XivPlugin/Modules/XivExpression.fs
@@ -10,7 +10,7 @@ open KPX.TheBot.Host.Utils.RecipeRPN
 
 type ItemAccumulator = ItemAccumulator<XivItem>
 
-type XivExpression () as x =
+type XivExpression() as x =
     inherit RecipeExpression<XivItem>()
 
     do
@@ -28,4 +28,44 @@ type XivExpression () as x =
         x.Operators.Add(itemOperator)
 
     override x.TryGetItemByName(str) =
+        failwith ""
+        // 不再使用
         ItemCollection.Instance.TryGetByName(str.TrimEnd(CommandUtils.XivSpecialChars))
+
+    override x.Tokenize(token) =
+        match token with
+        | _ when String.forall Char.IsDigit token -> Operand(Number(token |> float))
+        | _ ->
+            if token.Contains("@") then
+                let tmp = token.Split('@')
+
+                if tmp.Length <> 2 then
+                    failwithf "装等表达式错误"
+
+                let job = ClassJobMapping.TryParse(tmp.[0])
+                let iLevel = int tmp.[1]
+                let cgc = CraftableGearCollection.Instance
+
+                if job.IsNone then
+                    failwith "未知职业，如确定无误请联系开发者添加简写"
+
+                let items =
+                    cgc.Search(iLevel, job.Value)
+                    |> Array.map (fun g -> ItemCollection.Instance.GetByItemId(g.ItemId))
+
+                if items.Length = 0 then
+                    failwithf "不存在指定的装备"
+
+                let acu = ItemAccumulator()
+
+                for item in items do
+                    acu.Update(item)
+
+                Operand(Accumulator(acu))
+            else
+                let item = ItemCollection.Instance.TryGetByName(token.TrimEnd(CommandUtils.XivSpecialChars))
+
+                if item.IsNone then
+                    failwithf $"找不到物品 %s{token}"
+
+                Operand(Accumulator(ItemAccumulator.SingleItemOf(item.Value)))
--- src/XivPlugin/Modules/XivModule.fs
+++ src/XivPlugin/Modules/XivModule.fs
@@ -32,12 +32,6 @@ type SeaFishingOption() as x =
 type MiscModule() =
     inherit CommandHandlerBase()
 
-    let isNumber (str: string) =
-        if str.Length <> 0 then
-            String.forall Char.IsDigit str
-        else
-            false
-
     let chsDfcRoulettes = lazy (XivContentCollection.Instance.GetDailyFrontline(VersionRegion.China))
     let officalDfcRoulettes = lazy (XivContentCollection.Instance.GetDailyFrontline(VersionRegion.Offical))
 
@@ -144,47 +138,6 @@ type MiscModule() =
     member x.TestXivFantasia() =
         let tc = TestContext(x)
         tc.ShouldNotThrow("#幻想药")
-
-    [<CommandHandlerMethod("#cgss",
-                           "查找指定职业和品级的套装。用于#r/rr/rc/rrc计算",
-                           "#cgss 职业 品级
-勉强能用。也不打算改")>]
-    member x.HandleCGSS(cmdArg: CommandEventArgs) =
-        let mutable job = None
-        let mutable iLv = None
-
-        for item in cmdArg.HeaderArgs do
-            if isNumber item then
-                iLv <- Some(Int32.Parse(item))
-            else
-                let ret = ClassJobMapping.TryParse(item.ToUpperInvariant())
-
-                if ret.IsSome then
-                    job <- Some(ret.Value)
-
-        if job.IsNone then
-            cmdArg.Abort(InputError, "没有职业信息。职业可以使用：单字简称/全程/英文简称")
-
-        if iLv.IsNone then
-            cmdArg.Abort(InputError, "没有品级信息")
-
-        let cgc = CraftableGearCollection.Instance
-
-        let items =
-            cgc.Search(iLv.Value, job.Value)
-            |> Array.map (fun g -> ItemCollection.Instance.GetByItemId(g.ItemId))
-
-        if items.Length <> 0 then
-            let byId = items |> Seq.map (fun item -> $"#%i{item.ItemId}")
-            let byName = items |> Seq.map (fun item -> item.DisplayName)
-            cmdArg.Reply($"{String.Join('+', byId)}\r\n{String.Join('+', byName)}")
-        else
-            cmdArg.Reply("没找到")
-
-    [<TestFixture>]
-    member x.TestXivCGSS() =
-        let tc = TestContext(x)
-        tc.ShouldNotThrow("#cgss 占星 510")
 
     (*
     [<CommandHandlerMethod("#is", "（FF14）查找名字包含字符的物品", "关键词（大小写敏感）")>]
