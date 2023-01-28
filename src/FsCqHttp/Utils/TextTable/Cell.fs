2c9c10dc9d52c87b8193c77818cdc7737ad2a001
KPX <dm.mikufans@gmail.com>
2023-01-28 19:20:12 +0800

    TheBot : 调整项目框架到.NET 7。使用Generic math优化TextTable生成。

--- src/FsCqHttp/Utils/TextTable/Cell.fs
+++ src/FsCqHttp/Utils/TextTable/Cell.fs
@@ -1,165 +1,64 @@
 namespace KPX.FsCqHttp.Utils.TextResponse
 
 open System
-open System.Collections.Generic
+open System.Numerics
 
-
-module private CellBuildImpl =
-
-    let inline trimDigits (str : string) =
-        if str.EndsWith(".00") then
-            TableCell(str.Remove(str.Length - 3), Align = TextAlignment.Right)
-        else
-            TableCell(str, Align = TextAlignment.Right)
-
-    let inline numberImpl (value: ^Number)=
-        String.Format("{0:N2}", value)
-        |> trimDigits
-
-    let inline humanReadbleImpl (value: ^Number) (ignoreDigits : bool)=
-        let sigDigits = 4
-
-        let mutable value = TableCellHelper.RoundSigDigits(Convert.ToDouble(value), sigDigits)
-
-        let str =
-            match value with
-            | 0.0 -> "0"
-            | _ when Double.IsNaN(value) -> "NaN"
-            | _ when Double.IsNegativeInfinity(value) -> "+inf%"
-            | _ when Double.IsPositiveInfinity(value) -> "-inf%"
-            | _ ->
-                let pow10 = ((value |> abs |> log10 |> floor) + 1.0) |> floor |> int
-
-                let scale, postfix = if pow10 >= 9 then 8.0, "亿" else 0.0, ""
-
-                let hasEnoughDigits = (pow10 - (int scale) + 1) >= sigDigits
-
-                if hasEnoughDigits || ignoreDigits then
-                    String.Format("{0:N0}{1}", value / 10.0 ** scale, postfix)
-                else
-                    String.Format("{0:N2}{1}", value / 10.0 ** scale, postfix)
-
-        TableCell(str, Align = TextAlignment.Right)
-
-// Widening happens automatically for int32 to int64, int32 to nativeint, and int32 to double
-// 需要int32, int64, float
 
 [<AutoOpen; AbstractClass; Sealed>]
 type CellUtils private () =
 
-    /// 使用ToString()的字符串表示
-    static member inline Literal(item: int32) = TableCell(item.ToString())
-
-    /// 使用ToString()的字符串表示
-    static member inline Literal(item: uint32) = TableCell(item.ToString())
-
-    /// 使用ToString()的字符串表示
-    static member inline Literal(item: int64) = TableCell(item.ToString())
-
-    /// 使用ToString()的字符串表示
-    static member inline Literal(item: uint64) = TableCell(item.ToString())
-
-    /// 使用ToString()的字符串表示
-    static member inline Literal(item: float) = TableCell(item.ToString())
+    static member inline Literal<'Number when INumber<'Number>>(item: 'Number) = TableCell(item.ToString())
 
     static member inline Literal(str: string) = TableCell(str)
 
-    /// 使用ToString()的字符串表示
-    static member inline RLiteral(item: int32) =
-        TableCell(item.ToString(), Align = TextAlignment.Right)
-
-    /// 使用ToString()的字符串表示
-    static member inline RLiteral(item: uint32) =
-        TableCell(item.ToString(), Align = TextAlignment.Right)
-
-    /// 使用ToString()的字符串表示
-    static member inline RLiteral(item: int64) =
-        TableCell(item.ToString(), Align = TextAlignment.Right)
-
-    /// 使用ToString()的字符串表示
-    static member inline RLiteral(item: uint64) =
-        TableCell(item.ToString(), Align = TextAlignment.Right)
-
-    /// 使用ToString()的字符串表示
-    static member inline RLiteral(item: float) =
+    static member inline RLiteral<'Number when INumber<'Number>>(item: 'Number) =
         TableCell(item.ToString(), Align = TextAlignment.Right)
 
     static member inline RLiteral(str: string) =
         TableCell(str, Align = TextAlignment.Right)
 
     /// 右对齐，千分位
-    static member inline Integer(value: int32) =
-        TableCell(String.Format("{0:N0}", value), Align = TextAlignment.Right)
-
-    /// 右对齐，千分位
-    static member inline Integer(value: int64) =
-        TableCell(String.Format("{0:N0}", value), Align = TextAlignment.Right)
-
-    /// 右对齐，千分位
-    static member inline Integer(value: float) =
+    static member inline Integer<'Number when INumber<'Number>>(value: 'Number) =
         TableCell(String.Format("{0:N0}", value), Align = TextAlignment.Right)
 
     /// 右对齐，千分位，2位小数
-    static member inline Float(value: int32) =
-        TableCell(String.Format("{0:N2}", value), Align = TextAlignment.Right)
-
-    /// 右对齐，千分位，2位小数
-    static member inline Float(value: int64) =
-        TableCell(String.Format("{0:N2}", value), Align = TextAlignment.Right)
-
-    /// 右对齐，千分位，2位小数
-    static member inline Float(value: float) =
+    static member inline Float<'Number when INumber<'Number>>(value: 'Number) =
         TableCell(String.Format("{0:N2}", value), Align = TextAlignment.Right)
 
     /// 右对齐，千分位，不为零则保留2位小数
-    static member inline Number(value: float) = CellBuildImpl.numberImpl value
+    static member inline Number<'Number when INumber<'Number>>(value: 'Number) =
+        let str = String.Format("{0:N2}", value)
 
-    /// 右对齐，千分位，不为零则保留2位小数
-    static member inline Number(value: int32) = CellBuildImpl.numberImpl value
-
-    /// 右对齐，千分位，不为零则保留2位小数
-    static member inline Number(value: int64) = CellBuildImpl.numberImpl value
-
-    /// 右对齐，千分位，保留4位有效数字，不为零则保留2位小数
-    static member Sig4(value: float) =
-        TableCellHelper.RoundSigDigits(value, 4) |> Number
+        if str.EndsWith(".00") then
+            TableCell(str.Remove(str.Length - 3), Align = TextAlignment.Right)
+        else
+            TableCell(str, Align = TextAlignment.Right)
 
     /// 右对齐，千分位，保留4位有效数字，不为零则保留2位小数
-    static member Sig4(value: int32) =
-        TableCellHelper.RoundSigDigits(value, 4) |> Number
-
-    /// 右对齐，千分位，保留4位有效数字，不为零则保留2位小数
-    static member Sig4(value: int64) =
+    static member inline Sig4<'Number when INumber<'Number> and 'Number: equality>(value: 'Number) =
         TableCellHelper.RoundSigDigits(value, 4) |> Number
 
     /// 右对齐，千分位，保留4位有效数字，超过一亿按亿计，不为零则保留2位小数
-    static member HumanSig4(value: float) = CellBuildImpl.humanReadbleImpl (value) (false)
-
-    /// 右对齐，千分位，保留4位有效数字，超过一亿按亿，不为零则保留2位小数
-    static member HumanSig4(value: int32) = CellBuildImpl.humanReadbleImpl (value)  (false)
-
-    /// 右对齐，千分位，保留4位有效数字，超过一亿按亿，不为零则保留2位小数
-    static member HumanSig4(value: int64) = CellBuildImpl.humanReadbleImpl (value)  (false)
+    static member inline HumanSig4<'Number when INumber<'Number>>(value: 'Number) =
+        TableCellHelper.HumanReadble(value, false)
 
     /// 右对齐，千分位，保留4位有效数字，超过一亿按亿计，忽略原始数值的小数点
-    static member HumanSig4I(value: float) = CellBuildImpl.humanReadbleImpl (round value)  (true)
-
-    /// 右对齐，千分位，保留4位有效数字，超过一亿按亿计，忽略原始数值的小数点
-    static member HumanSig4I(value: int32) = CellBuildImpl.humanReadbleImpl (value) (true)
-
-    /// 右对齐，千分位，保留4位有效数字，超过一亿按亿计，忽略原始数值的小数点
-    static member HumanSig4I(value: int64) = CellBuildImpl.humanReadbleImpl (value) (true)
+    static member inline HumanSig4I<'Number when INumber<'Number>>(value: 'Number) =
+        let value = Convert.ToDouble(value)
+        TableCellHelper.HumanReadble(value, true)
 
     /// 2位小数百分比
     static member inline Percent(value: float) =
         TableCell(String.Format("{0:P2}", value), Align = TextAlignment.Right)
 
     /// 转换为时间跨度，无效值记为"--"
-    static member TimeSpan(value: DateTimeOffset) =
+    static member inline TimeSpan(value: DateTimeOffset) =
         let str =
-            if value = DateTimeOffset.MaxValue
-               || value = DateTimeOffset.MinValue
-               || value = DateTimeOffset.UnixEpoch then
+            if
+                value = DateTimeOffset.MaxValue
+                || value = DateTimeOffset.MinValue
+                || value = DateTimeOffset.UnixEpoch
+            then
                 "--"
             else
                 TableCellHelper.FormatTimeSpan(DateTimeOffset.Now - value)
@@ -167,7 +66,7 @@ type CellUtils private () =
         TableCell(str, Align = TextAlignment.Right)
 
     /// 转换为时间跨度，无效值记为"--"
-    static member TimeSpan(value: TimeSpan) =
+    static member inline TimeSpan(value: TimeSpan) =
         TableCell(TableCellHelper.FormatTimeSpan(value), Align = TextAlignment.Right)
 
     /// 转换为日期格式yyyy/MM/dd HH:mm
@@ -175,11 +74,11 @@ type CellUtils private () =
         TableCell(value.ToLocalTime().ToString("yyyy/MM/dd HH:mm"), Align = TextAlignment.Right)
 
     /// 隐蔽右对齐单元格。在图片中透明，在文本中为空格
-    static member RightPad =
+    static member inline RightPad =
         TableCell(String.Empty, RenderMode = RenderMode.IgnoreIfImage, Align = TextAlignment.Right)
 
     /// 隐蔽左对齐单元格。在图片中透明，在文本中为空格
-    static member LeftPad = TableCell(String.Empty, RenderMode = RenderMode.IgnoreIfImage, Align = TextAlignment.Left)
+    static member inline LeftPad = TableCell(String.Empty, RenderMode = RenderMode.IgnoreIfImage, Align = TextAlignment.Left)
 
     /// 拆分多行文本，默认格式
     static member inline SplitTextRows(str: string) =
--- src/FsCqHttp/Utils/TextTable/TableCellHelper.fs
+++ src/FsCqHttp/Utils/TextTable/TableCellHelper.fs
@@ -1,35 +1,12 @@
 namespace KPX.FsCqHttp.Utils.TextResponse
 
 open System
+open System.Numerics
 
 
-type internal TableCellHelper private () =
-    static let numberTypes =
-        seq {
-            typeof<sbyte>
-            typeof<byte>
-            typeof<int16>
-            typeof<uint16>
-            typeof<int32>
-            typeof<uint32>
-            typeof<int64>
-            typeof<uint64>
-            typeof<bigint>
-            typeof<float32>
-            typeof<float>
-            typeof<decimal>
-        }
-        |> Seq.map (fun t -> t.FullName)
-        |> set
-
-    static member EnsureNumberType(item: obj) =
-        let name = item.GetType().FullName
-
-        if not <| numberTypes.Contains(name) then
-            raise <| InvalidOperationException($"需要数值类型，而给定的是%s{name}")
-
-    static member RoundSigDigits(number: obj, sigDigits: int) =
-        let value = Convert.ToDouble(number)
+type TableCellHelper =
+    static member RoundSigDigits<'Number when INumber<'Number> and 'Number: equality>(value: 'Number, sigDigits: int) =
+        let value = Convert.ToDouble(value)
 
         if value = 0.0 then
             0.0
@@ -41,11 +18,32 @@ type internal TableCellHelper private () =
             scale * Math.Round(value / scale, sigDigits)
 
     static member FormatTimeSpan(ts: TimeSpan) =
-        if ts = TimeSpan.MaxValue then
-            "--"
-        elif ts.TotalDays >= 1.0 then
-            $"%.0f{ts.TotalDays}天前"
-        elif ts.TotalHours >= 1.0 then
-            $"%.0f{ts.TotalHours}时前"
-        else
-            $"%.0f{ts.TotalMinutes}分前"
+        if ts = TimeSpan.MaxValue then "--"
+        elif ts.TotalDays >= 1.0 then $"%.0f{ts.TotalDays}天前"
+        elif ts.TotalHours >= 1.0 then $"%.0f{ts.TotalHours}时前"
+        else $"%.0f{ts.TotalMinutes}分前"
+
+    static member HumanReadble<'Number when INumber<'Number>>(value: 'Number, ignoreDigits: bool) =
+        let sigDigits = 4
+
+        let mutable value = TableCellHelper.RoundSigDigits(Convert.ToDouble(value), sigDigits)
+
+        let str =
+            match value with
+            | 0.0 -> "0"
+            | _ when Double.IsNaN(value) -> "NaN"
+            | _ when Double.IsNegativeInfinity(value) -> "+inf%"
+            | _ when Double.IsPositiveInfinity(value) -> "-inf%"
+            | _ ->
+                let pow10 = ((value |> abs |> log10 |> floor) + 1.0) |> floor |> int
+
+                let scale, postfix = if pow10 >= 9 then 8.0, "亿" else 0.0, ""
+
+                let hasEnoughDigits = (pow10 - (int scale) + 1) >= sigDigits
+
+                if hasEnoughDigits || ignoreDigits then
+                    String.Format("{0:N0}{1}", value / 10.0 ** scale, postfix)
+                else
+                    String.Format("{0:N2}{1}", value / 10.0 ** scale, postfix)
+
+        TableCell(str, Align = TextAlignment.Right)
