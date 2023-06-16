08a6fa94359ae00381d82349177697c590976ae4
KPX <dm.mikufans@gmail.com>
2023-06-16 23:28:27 +0800

    TheBot : RecipeManager中非Rec方法不再检查Expand

--- src/TheBot/DataModel/RecipeManager2.fs
+++ src/TheBot/DataModel/RecipeManager2.fs
@@ -7,7 +7,7 @@ type IRecipeProvider<'Item, 'Recipe when 'Item: equality> =
 type IRecipeConfig<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipeProcess<'Item>> =
     /// 所需数量不足1流程时计算方式
     abstract RunRounding: ProcessRunRounding
-    /// 判断该配方能否被展开
+    /// 判断该配方能否被展开，用于Rec计算
     abstract CanExpandRecipe: 'Recipe -> bool
     /// 当出现多个配方时，选择一个进行后续计算
     abstract SolveSameProduct: seq<'Recipe> -> 'Recipe
@@ -32,10 +32,7 @@ type RecipeConfig<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipeProc
 
 [<AbstractClass>]
 type RecipeManager<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipeProcess<'Item>>
-    (
-        providers: seq<IRecipeProvider<'Item, 'Recipe>>,
-        cfg: IRecipeConfig<'Item, 'Recipe>
-    ) =
+    (providers: seq<IRecipeProvider<'Item, 'Recipe>>, cfg: IRecipeConfig<'Item, 'Recipe>) =
 
     /// 配方 数量 展开层数
     abstract ApplyProcessQuantity: 'Recipe * ProcessQuantity * int -> IRecipeProcess<'Item>
@@ -43,24 +40,29 @@ type RecipeManager<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipePro
     member x.Config = cfg
 
     /// 从Provider查找所有配方，需要满足CanExpand
-    member x.GetRecipes(item: 'Item) =
-        providers
-        |> Seq.choose (fun p ->
-            p.TryGetRecipe(item)
-            |> Option.filter (fun recipe -> cfg.CanExpandRecipe(recipe)))
-        |> Seq.toList
+    member x.GetRecipes(item: 'Item, ?checkExpand: bool) =
+        let check = defaultArg checkExpand false
+
+        if check then
+            providers
+            |> Seq.choose (fun p ->
+                p.TryGetRecipe(item)
+                |> Option.filter (fun recipe -> cfg.CanExpandRecipe(recipe)))
+            |> Seq.toList
+        else
+            providers |> Seq.choose (fun p -> p.TryGetRecipe(item)) |> Seq.toList
 
     /// 从Provider查找所有配方
-    member x.GetRecipe(item: 'Item) =
-        let ret = x.GetRecipes(item)
+    member x.GetRecipe(item: 'Item, ?checkExpand: bool) =
+        let ret = x.GetRecipes(item, ?checkExpand = checkExpand)
 
         if ret.Length = 0 then
             invalidOp $"找不到{item}的制作配方"
 
         ret |> cfg.SolveSameProduct
 
-    member x.TryGetRecipe(item: 'Item) =
-        let ret = x.GetRecipes(item)
+    member x.TryGetRecipe(item: 'Item, ?checkExpand: bool) =
+        let ret = x.GetRecipes(item, ?checkExpand = checkExpand)
 
         if ret.Length = 0 then
             None
@@ -123,7 +125,7 @@ type RecipeManager<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipePro
                 if depth >= depthLimit then
                     acc.Materials.Update(mr.Item, required)
                 else
-                    match x.TryGetRecipe(mr.Item) with
+                    match x.TryGetRecipe(mr.Item) with ////////////////
                     | None ->
                         if depth = RecipeManager.DEPTH_PRODUCT then
                             invalidOp $"输入物品%A{mr.Item} 没有生产配方"
@@ -162,7 +164,7 @@ type RecipeManager<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipePro
           RelatedItems = items |> Seq.toArray }
 
     member x.TryGetMaterialsRec(mr: RecipeMaterial<'Item>, ?inv: MaterialInventory<'Item>, ?depthLimit: int) =
-        let materials = x.TryGetMaterials(mr)
+        let materials = x.TryGetMaterials(mr) ////////////////
 
         if materials.IsSome then
             Some(x.GetMaterialsRec(Seq.singleton mr, ?inv = inv, ?depthLimit = depthLimit))
@@ -170,10 +172,12 @@ type RecipeManager<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipePro
             None
 
 type MaterialsRecContext<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipeProcess<'Item>> =
-    { FinalProcess: IRecipeProcess<'Item>
-      /// 注意：这里的配方都是原始配方
-      IntermediateProcess: IntermediateProcessInfo<'Item, 'Recipe> []
-      RelatedItems: 'Item [] }
+    {
+        FinalProcess: IRecipeProcess<'Item>
+        /// 注意：这里的配方都是原始配方
+        IntermediateProcess: IntermediateProcessInfo<'Item, 'Recipe>[]
+        RelatedItems: 'Item[]
+    }
 
 type IntermediateProcessInfo<'Item, 'Recipe when 'Item: equality and 'Recipe :> IRecipeProcess<'Item>> =
     { Quantity: ProcessQuantity
